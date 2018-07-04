1c92bc82384173b46d10bdb9d1e36a3821a12b3d
linyang@offcn.com <lin2798003@sina.com>
2018-07-04 09:07:51 +0800

    优化初始化脚本

--- init.sh
+++ init.sh
@@ -20,4 +20,5 @@ echo "redis.session.database = 0" >> ${APP_PATH}/conf/application.ini && \
 echo "redis.session.password = \"${REDIS_PASSWORD}\"" >> ${APP_PATH}/conf/application.ini && \
 echo "redis.session.timeout = 2" >> ${APP_PATH}/conf/application.ini
 
+#初始化监听脚本
 php ${APP_PATH}/bin/cli console/listen/index >> ${LOG_FILE} &
